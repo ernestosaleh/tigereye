5e59900e0dfb9f0b13cfd30b51f8b1f52ae2683b
Marco Tavares <matr192001@hotmail.com>
2023-05-28 11:37:35 -0600

    Nuevos

---
+++ 1_🌐_Inicio.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title("Maximice la Seguridad en Nuevo León con Nuestro Proyecto Innovador")
+
+st.markdown("""
+    Vivir en seguridad es un derecho fundamental, y en Nuevo León estamos comprometidos con asegurarnos de que cada ciudadano pueda disfrutar de este derecho. Sin embargo, la violencia puede manifestarse de muchas formas y puede ser difícil de entender y combatir sin las herramientas adecuadas. Aquí es donde entra nuestro proyecto.
+    \n¿Alguna vez se ha preguntado cuánto impacto tiene la violencia doméstica, la violencia de género, la violencia escolar, o la violencia en el trabajo en nuestra comunidad? ¿Cómo se comparan estas formas de violencia con el crimen organizado o las agresiones físicas en las calles? ¿Cómo podemos dirigir los recursos de manera más efectiva para combatir estas diferentes formas de violencia?
+    \nNuestro proyecto innovador desglosa la violencia en la Zona Metropolitana de Nuevo León en sus diferentes tipos, utilizando una gama de indicadores para dar una imagen detallada y matizada de la situación en nuestra región. Los indicadores no sólo nos ayudan a entender el alcance y la naturaleza de la violencia, sino que también pueden ser una herramienta valiosa para desarrollar e implementar estrategias de prevención efectivas.
+    \nLos datos y la información recopilada a través de estos indicadores son vitales para impulsar la toma de decisiones informada y efectiva. Esto nos permite priorizar áreas que necesitan atención urgente y también identificar patrones o tendencias en la violencia que pueden ser intervenidos de manera estratégica.
+    \nEn última instancia, el uso de estos indicadores es crucial para garantizar que todos los esfuerzos que se hagan para mejorar la seguridad en la Zona Metropolitana de Nuevo León sean basados en evidencia y estén orientados a las necesidades reales de nuestra comunidad.
+    \nCreemos que nuestro proyecto puede ayudar a Nuevo León a avanzar hacia un futuro más seguro y pacífico. Le invitamos a unirse a nosotros en este esfuerzo y a ayudar a hacer de la Zona Metropolitana de Nuevo León un lugar más seguro para todos. Porque cada paso que damos para entender mejor la violencia en nuestra comunidad es un paso que nos acerca a la solución.
+    """
+)
---
+++ pages/2_📝_Tipos de Violencia.py
@@ -0,0 +1,109 @@
+from datetime import datetime, date
+import pandas as pd
+import streamlit as st
+import folium
+import json
+from sqlalchemy import create_engine
+from streamlit_folium import folium_static
+import branca
+
+st.title("📝 Tipos de violencia.")
+
+hoy = datetime.now()
+
+col1, col2 = st.columns([3,1])
+
+#Create connection
+user="sugroup"
+password="fimenos_injustos69"
+host="sugroup.mysql.database.azure.com"
+port=3306
+database="prueba1"
+my_conn = create_engine(rf"mysql+mysqldb://{user}:{password}@{host}:{port}/{database}")
+
+query = f"SELECT * FROM violencia"
+result_df=pd.read_sql(query,my_conn)
+result_df["fecha"] = pd.to_datetime(result_df["fecha"]).dt.date
+
+violencia_rango = st.slider(
+    "Rango de tiempo:",
+    value=(datetime(2020, 1, 18), datetime(hoy.year, hoy.month, hoy.day)),
+    format=("MM/YY"),
+
+)
+
+cities = {
+    "Apodaca": "006", 
+    "García": "018", 
+    "San Pedro Garza García": "019",
+    "Escobedo": "021",
+    "Guadalupe": "026", 
+    "Juárez": "031",
+    "Monterrey": "039",
+    "San Nicolás de los Garza": "046",
+    "Santa Catarina": "048",
+    "Juarez": "031",
+    "Santiago": "049",
+}
+
+poblacion = {
+    "006": 656464.0, 
+    "018": 397205.0, 
+    "019": "NULL",
+    "021": 500000,
+    "026": 643143.0, 
+    "031": 500000,
+    "039": 1142994.0,
+    "046": 500000,
+    "048": 306322.0,
+    "049": 46784.0,
+}
+
+def calculate_values(violencia_rango,multiple, radio_valor):
+    mask = (result_df['fecha'] > pd.to_datetime(violencia_rango[0])) & (result_df['fecha'] <= pd.to_datetime(violencia_rango[1]))
+    postdf = result_df.loc[mask]
+    filtered_df = postdf[postdf['tipo'].isin(multiple)]
+    filtered_df = filtered_df.groupby(['lugar'])['valor'].sum().reset_index()
+    filtered_df["clave_municipio"]=filtered_df["lugar"].apply(lambda x: cities[x])
+    filtered_df["percapita"] = (filtered_df["valor"] / filtered_df["clave_municipio"].map(poblacion)) * 1000
+    variable = []
+
+    if radio_valor == "Percapita":
+        variable = [int(filtered_df["percapita"].max()), "percapita"]
+    else:
+        variable = [int(filtered_df["valor"].max()), "valor"]
+
+    colormap = branca.colormap.linear.YlOrRd_09.scale(0, variable[0])
+    map = folium.Map(location=[25.651887, -100.370891], zoom_start=10)
+
+    folium.Choropleth(
+        geo_data="municipioszmm.geojson", 
+        name='choropleth',
+        data=filtered_df,
+        columns=['clave_municipio', variable[1]],
+        key_on='feature.properties.clave_municipio', 
+        fill_color='YlOrRd',
+        fill_opacity=0.7,
+        line_opacity=0.2,
+        legend_name='Violencia (%)'
+    ).add_to(map)
+    
+    folium_static(map)
+   
+
+with col2:
+    radio_valor = st.radio(
+        "Rasgo a considerar",
+        ('N. de personas', 'Percapita'),
+        key="N. de personas",
+    )
+
+with col1:
+    choice = ['Patrimonio', 'Familia', 'Libertad y Seguridad Sexual', 'Sociedad', 'Vida e Integridad Corporal', 'Libertad Personal', 'Otros Bienes Jurídicos Afectados (Del Fuero Común)']
+    multiple = st.multiselect(
+        'Violencia en:',
+        choice,
+        choice
+    )
+
+calculate_values(violencia_rango,multiple, radio_valor)
